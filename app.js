2dd8862380725281f757844de22295af7145c67a
n0b136 <50467142+N0b136@users.noreply.github.com>
2019-07-22 18:20:44 -0500

    Completed steps 3-5

--- app.js
+++ app.js
@@ -5,3 +5,23 @@ let person1 = {
 
 person1.sayHello();
 
+function Person (name, city, age) {
+    this.name = name,
+    this.city = city,
+    this.age = age
+}
+
+Person.prototype.sayHello = function () {
+    console.log(`Hello! my name is ${this.name}, I am ${this.age}, and I live in ${this.city}.`);
+};
+
+let p1 = new Person('Rae','Nashville', 27);
+let p2 = new Person('Boyo', 'Houston', 23);
+let p3 = new Person('Seth', 'St. Louis', 29);
+let p4 = new Person('Levi', 'Nashville', 2);
+let p5 = new Person('Haddie', 'Nashville', 1);
+p1.sayHello();
+p2.sayHello();
+p3.sayHello();
+p4.sayHello();
+p5.sayHello();
